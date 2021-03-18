fd179dd41e859208e01e2c43c4c3b3a1d7ade9e0
AGABAHENRY <41643387+AGABAHENRY@users.noreply.github.com>
2021-03-18 14:16:11 +0300

    --- Auto-Git Commit ---

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -16,12 +16,39 @@ class ProductSeeder extends Seeder
     public function run()
     {
         //
-        DB::table('_products')->insert([
-            'name'=>'LG',
-            'price'=>'$200',
-            'category'=>'mobile',
-            'description'=>'A smart phone with 4GB Ram and much more features',
-            'gallery'=>'https://www.lg.com/in/mobile-phones/lg-lmg910emw-aurora-silver',
-        ]);
+        DB::table('_products')->insert(
+            [
+              
+                [
+                    'name'=>'oppo',
+                    'price'=>'$300',
+                    'category'=>'mobile',
+                    'description'=>'A smart phone with 8GB Ram and much more features',
+                    'gallery'=>'https://www.lg.com/in/mobile-phones/lg-lmg910emw-aurora-silver',
+                ],
+                [
+                    'name'=>'LG',
+                    'price'=>'$200',
+                    'category'=>'mobile',
+                    'description'=>'A smart phone with 4GB Ram and much more features',
+                    'gallery'=>'https://www.lg.com/in/mobile-phones/lg-lmg910emw-aurora-silver',
+                ],
+                [
+                    'name'=>'LG',
+                    'price'=>'$200',
+                    'category'=>'mobile',
+                    'description'=>'A smart phone with 4GB Ram and much more features',
+                    'gallery'=>'https://www.lg.com/in/mobile-phones/lg-lmg910emw-aurora-silver',
+                ],
+                [
+                    'name'=>'LG',
+                    'price'=>'$200',
+                    'category'=>'mobile',
+                    'description'=>'A smart phone with 4GB Ram and much more features',
+                    'gallery'=>'https://www.lg.com/in/mobile-phones/lg-lmg910emw-aurora-silver',
+                ]
+                
+            ]
+        );
     }
 }
