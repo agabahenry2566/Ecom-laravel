0f727e07dfb81602e117e4215dc691d116c6b555
AGABAHENRY <41643387+AGABAHENRY@users.noreply.github.com>
2021-03-18 13:16:01 +0300

    --- Auto-Git Commit ---

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('users')->insert([
+            'name'=>'LG',
+            'price'=>'$200',
+            'category'=>'mobile',
+            'description'=>'A smart phone with 4GB Ram and much more features',
+            'gallery'=>'https://www.lg.com/in/mobile-phones/lg-lmg910emw-aurora-silver',
+        ]);
+    }
+}
